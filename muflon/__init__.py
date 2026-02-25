3ee018de58d9dd674a0c5b263e812a554959dc4d
Jackob K <148371832+Kiryl24@users.noreply.github.com>
2026-02-25 01:34:15 +0100

    -
    -

--- muflon/__init__.py
+++ muflon/__init__.py
@@ -1,3 +1,3 @@
 from .io import parse_data_to_matrices, save_results_to_csv
-from .operations import fuzzy_composition, solve_vector
+from .operations import fuzzy_composition_multi, solve_fuzzy_vector, get_reduced_matrix, get_binarized_matrix, find_minimal_vectors
 from .norms import get_norm
